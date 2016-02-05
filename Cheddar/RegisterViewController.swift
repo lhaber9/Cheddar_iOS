c4fa8c9b6891dd23e7fe091521c2da4471c4956b
Lucas Haber <lhaber9@gmail.com>
2016-02-04 23:12:45 -0500

    fix for little bug

--- Cheddar/RegisterViewController.swift
+++ Cheddar/RegisterViewController.swift
@@ -13,6 +13,7 @@ class RegisterViewController: FrontPageViewController {
     @IBOutlet var emailTextField: UITextField!
     
     @IBAction func goToNext() {
+        tapOut()
         if (isEmailVaildated()) {
             delegate?.goToNextPageWithController(SelectionViewController())
         }
