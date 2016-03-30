52ff167aab323ec14d9b7a624a7500312c2dbc39
Lucas Haber <lhaber9@gmail.com>
2016-03-29 20:25:55 -0400

    increase delay time to see animation. change order of images

--- Cheddar/ViewController.swift
+++ Cheddar/ViewController.swift
@@ -186,7 +186,7 @@ class ViewController: UIViewController, UIScrollViewDelegate, FrontPageViewDeleg
             self.loadingView.alpha = 1
         }
         
-        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(3 * Double(NSEC_PER_SEC)))
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(5 * Double(NSEC_PER_SEC)))
         dispatch_after(delayTime, dispatch_get_main_queue()) {
             callback()
         }
